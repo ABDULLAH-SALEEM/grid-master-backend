f7204ee82960cd0bc4ceea41f82696165151121e
Abdullah Saleem <saleemabdullah764@gmail.com>
2025-01-15 19:50:17 +0100

    changed port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./pre-start"; // Must be the first import
 import logger from "jet-logger";
 import server from "./server";
 
-const port = parseInt(process.env.PORT || "10000", 10);
+const port = parseInt(process.env.PORT || "8080", 10);
 const SERVER_START_MSG = "Express server started on port: " + port;
 
 server.listen(port, "0.0.0.0", () => logger.info(SERVER_START_MSG));
